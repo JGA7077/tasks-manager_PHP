32f98c7dea25843c9b6014e9f508bdbdc2bb5686
João Gabriel <joaogabriel-al@hotmail.com>
2025-01-25 10:09:44 -0300

    creating Task class

---
+++ Task.php
@@ -0,0 +1,27 @@
+<?php 
+class Task {
+  private $title, $description;
+
+  public function __construct($title, $description) {
+    $this->setTitle($title);
+    $this->setDescription($description);
+  }
+
+  public function getTitle()
+  {
+    return $this->title;
+  }
+  public function setTitle($title)
+  {
+    $this->title = $title;
+  } 
+  public function getDescription()
+  {
+    return $this->description;
+  }
+  public function setDescription($description)
+  {
+    $this->description = $description;
+  }
+}
+?>
