2925942e7d26d4f28901b28722d6e38dd18ff8f2
João Gabriel <joaogabriel-al@hotmail.com>
2025-01-26 20:43:11 -0300

    creating task on JSON

--- Task.php
+++ Task.php
@@ -1,10 +1,25 @@
 <?php 
 class Task {
-  private $title, $description;
+  private $title, $description, $id;
 
-  public function __construct($title, $description) {
+  public function __construct($title, $description, $id) {
     $this->setTitle($title);
     $this->setDescription($description);
+    $this->setId($id);
+  }
+  public function saveTaskOnJSON() {
+    $task = [
+      "title" => $this->getTitle(),
+      "description" => $this->getDescription(),
+      "id" => $this->getId()
+    ];
+
+    $file = "../tasks.json";
+    $tasks = file_exists($file) ? json_decode(file_get_contents($file), true) : null;
+
+    $tasks["tasks"][] = $task;
+
+    file_put_contents($file, json_encode($tasks, JSON_PRETTY_PRINT));
   }
 
   public function getTitle()
@@ -22,6 +37,14 @@ class Task {
   public function setDescription($description)
   {
     $this->description = $description;
+  }
+  public function getId()
+  {
+    return $this->id;
+  }
+  public function setId($id)
+  {
+    $this->id = $id;
   }
 }
 ?>
