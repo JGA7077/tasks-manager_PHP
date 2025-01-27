ba8b198f9ae1999ee81f25f3ba4758bf81d25339
João Gabriel <joaogabriel-al@hotmail.com>
2025-01-27 13:30:28 -0300

    adding configs to avoid localhost error

--- tasks/getTasks.php
+++ tasks/getTasks.php
@@ -1,4 +1,8 @@
 <?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 if ($_SERVER["REQUEST_METHOD"] !== "GET") {
   http_response_code(405);
   echo json_encode(["error" => "Only GET requests are allowed"]);
